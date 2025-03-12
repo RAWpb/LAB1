1da83134c4834a6f10854f5938d6fb3eadbb6ac9
Каковин Никита <Yolochiller.pb@gmail.com>
2025-03-12 09:47:02 +0700

    Circle

---
+++ circle.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Функция для вычисления длинны окружности
+double calcLenght(double r) {
+    return (2 * 3.14 * r);
+}
+
+// Функция для вычисления площади круга
+double calcArea(double r) {
+    return (r * r * 3.14);
+}
+
+// Функция для вычисления площади кругового сектора
+double calcSecArea(double r, double s) {
+    double sector = (360 / s);
+    return (r * r * 3.14) / sector;
+}
+
+
+int main() {
+    double r, s;
+
+    // Ввод данных
+    cout << "Введите радиус окружности и значение кругового сектора в градусах (360 для целого круга)";
+    cin >> r >> s;
+
+    // Вычисления
+    double lenght = calcLenght(r);
+    double area = calcArea(r);
+    double secArea = calcSecArea(r, s);
+
+    // Вывод результатов
+    cout << "Длинна окружности: " << lenght << endl;
+    cout << "Площадь круга: " << area << endl;
+    cout << "Площадь кругового сектора: " << secArea << endl;
+
+    return 0;
+}
+
