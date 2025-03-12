116861dad6ef2248a845ee8d8f1e92bb6bab32c8
gahan135 <diana_aleev123@mail.ru>
2025-03-12 11:04:54 +0700

    Trapecia

---
+++ trapec.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int a, b, c, d, h;
+    setlocale(LC_ALL, "");
+    std::cout << "Введите стороны трапеции\n";
+    std::cin >> a >> b >> c >> d;
+
+    std::cout << "Введите стороны высоту\n";
+    std::cin >> h;
+    int v;
+    std::cout << "Выберите какую операцию совершить\n" << "1. Периметр\n" << "2. Площадь\n" << "3. Длина средней диагонали\n";
+    int z;
+    std::cin >> z;
+    switch (z)
+    {
+    case(1):
+        v = a + b + c + d;
+        std::cout << v;
+        break;
+    case(2):
+        v = ((a + b) / 2) * h;
+        std::cout << v;
+        break;
+    case(3):
+        v = (a + b) / 2;
+        std::cout << v;
+        break;
+
+    default:
+        break;
+    }
+}
+
+
