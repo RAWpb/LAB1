75c27f55d2a0634b0c601a915247c40c3db27996
RAWpb <yolochiller.pb@gmail.com>
2025-03-26 09:17:14 +0700

    circ

--- circle.cpp
+++ circle.cpp
@@ -5,22 +5,21 @@ using namespace std;
 
 // Функция для вычисления длинны окружности
 double calcLenght(double r) {
-    if (r > 0) return (2 * 3.14 * r);
-    else return 0;
+    return (2 * 3.14 * r);
+   
 }
 
 // Функция для вычисления площади круга
 double calcArea(double r) {
-    if (r > 0) return (r * r * 3.14);
-    else return 0;
+    return (r * r * 3.14);
+    
 }
 
 // Функция для вычисления площади кругового сектора
 double calcSecArea(double r, double s) {
     double sector = (360 / s);
-    if (r > 0 and s > 0)
     return (r * r * 3.14) / sector;
-    else return 0;
+
 }
 
 
@@ -37,10 +36,12 @@ int main() {
     double secArea = calcSecArea(r, s);
 
     // Вывод результатов
-    cout << "Длинна окружности: " << lenght << endl;
-    cout << "Площадь круга: " << area << endl;
-    cout << "Площадь кругового сектора: " << secArea << endl;
-
+    if ((r < 0) or (361 < s) or (s < 0)) cout << "Неточное значение" << endl;
+    else {
+        cout << "Длинна окружности: " << lenght << endl;
+        cout << "Площадь круга: " << area << endl;
+        cout << "Площадь кругового сектора: " << secArea << endl;
+    }
     return 0;
 }
 
